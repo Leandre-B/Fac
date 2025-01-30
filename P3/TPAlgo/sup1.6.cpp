a31307d74acd83311f8100dab372f276532a8fc4
Léandre <leandrebodo@gmail.com>
2025-01-30 10:51:23 +0100

    Create sup1.6.cpp

---
+++ P3/TPAlgo/sup1.6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+void hanoiSolver(int nDisque, char tA, char tB, char tC){
+
+    //faut locked-in là...
+
+}
+
+
+int main(){
+
+    int nDisque;
+    char tA = 'A', tB='B', tC='C';
+
+    std::cout<<"Avec combien de disque voulez-vous jouer ? : ";
+    std::cin>>nDisque;
+
+
+    return 0;
+}
