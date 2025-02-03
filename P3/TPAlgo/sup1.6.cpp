2915ac2a483c8322ddf737ee751245940e259a52
Léandre <leandrebodo@gmail.com>
2025-02-03 13:50:15 +0100

    Update sup1.6.cpp

--- P3/TPAlgo/sup1.6.cpp
+++ P3/TPAlgo/sup1.6.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 
-void hanoiSolver(int nDisque, char tA, char tB, char tC){
+void hanoi(int n, char t1, char t2, char t3){
+    if(n>2)
+        hanoi(n-1,t1,t2,t3);
+    std::cout<<t1<<"-->"<<t3<<std::endl;
+    std::cout<<t1<<"-->"<<t2<<std::endl;
+    std::cout<<t3<<"-->"<<t2<<std::endl;
+    if(n==2){
+        std::cout<<t1<<"-->"<<t3<<std::endl;
+    }
+    hanoi(n,t3,t2,t1);
 
-    //faut locked-in là...
+
 
 }
 
@@ -11,10 +20,11 @@ void hanoiSolver(int nDisque, char tA, char tB, char tC){
 int main(){
 
     int nDisque;
-    char tA = 'A', tB='B', tC='C';
+    char t1= 'A', t2='B', t3='C';
 
     std::cout<<"Avec combien de disque voulez-vous jouer ? : ";
     std::cin>>nDisque;
+    hanoi(nDisque, t1,t2,t3);
 
 
     return 0;
