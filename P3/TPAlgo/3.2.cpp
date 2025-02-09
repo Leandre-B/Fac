35b2b63b9b5e28dfa770cbd2e4b153f6d65cda69
Léandre <leandrebodo@gmail.com>
2025-02-09 22:02:30 +0100

    Add files via upload

--- P3/TPAlgo/3.2.cpp
+++ P3/TPAlgo/3.2.cpp
@@ -74,17 +74,16 @@ void addAllFilm(Selection & selection,EnsFilm ensFilm){
 void selectionFilm(Selection & selection, EnsFilm ensFilm){
     std::string real;
     int anneeMax, anneeMin, dureeMax, dureeMin;
-
     std::cout<<"Veuillez choisir un/des films selon les critères suivants: "<<std::endl;
 
     std::cout<<"Nom du realisateur (entrer 'X' pour tous les selectionner) : ";
     std::cin>>real;
 
-    std::cout<<"Année de sortie maximale : ";
-    std::cin>>anneeMax;
-
     std::cout<<"Année de sortie minimale : ";
     std::cin>>anneeMin;
+
+    std::cout<<"Année de sortie maximale : ";
+    std::cin>>anneeMax;
 
     std::cout<<"Duree minimale (en min) : ";
     std::cin>>dureeMin;
@@ -104,9 +103,9 @@ void selectionFilm(Selection & selection, EnsFilm ensFilm){
 
             //delete selection.adrTabFilm[i]; //faut le faire ???
             selection.adrTabFilm[i]=nullptr;
+            //--selection.nbFilm;
             }
     }
-
 }
 
 void affSelection(Selection selection){
@@ -126,34 +125,83 @@ void enregistrerSelection(Selection & selection,std::string nomFic){
     fic.open(nomFic);
 
     if(fic.is_open()){
+        fic<<selection.nbFilm<<std::endl;
         for(int i = 0;i<selection.nbFilm;++i){
             if(selection.adrTabFilm[i]!=nullptr)
-                fic<<(*selection.adrTabFilm[i]).nomFilm<<" ("
-                <<(*selection.adrTabFilm[i]).nomReal<<", "
-                <<(*selection.adrTabFilm[i]).annee<<")"<<std::endl;
+                fic<<(*selection.adrTabFilm[i]).nomFilm<<std::endl
+                <<(*selection.adrTabFilm[i]).annee<<std::endl
+                <<(*selection.adrTabFilm[i]).nomReal<<std::endl
+                <<(*selection.adrTabFilm[i]).duree<<std::endl
+                <<(*selection.adrTabFilm[i]).langue<<std::endl;
         }
     }else{
         std::cout<<"PROBLEME LORS DE L'OUVERTURE DU FICHIER !"<<std::endl;
     }
 }
 
+int nbFilmCommuns(Selection selec1, Selection selec2){
+    int nbCommun=0;
+    int j;
+    bool trouve;
+
+    for(int i =0; i<selec1.nbFilm; ++i){
+        j=0;
+        trouve = false;
+        while(j<selec2.nbFilm and !trouve){
+            if(selec1.adrTabFilm[i]!=nullptr and selec2.adrTabFilm[j]!=nullptr)
+                if(selec1.adrTabFilm[i]==selec2.adrTabFilm[j]){
+                    trouve = true;
+                    ++nbCommun;
+                }
+            ++j;
+        }
+    }
+
+    return nbCommun;
+}
+
+void triSelection(Selection & selection){
+    //bubble sort pck flemme
+
+    Film* temp;
+
+    for(int i = 0;i<selection.nbFilm-1;++i){
+        for(int j = i+1;j<selection.nbFilm;++j){
+            if(selection.adrTabFilm[i]!=nullptr and selection.adrTabFilm[j]!=nullptr)
+                if((*selection.adrTabFilm[i]).annee>(*selection.adrTabFilm[j]).annee){
+                    temp = selection.adrTabFilm[j];
+                    selection.adrTabFilm[j]=selection.adrTabFilm[i];
+                    selection.adrTabFilm[i]=temp;
+                }
+
+        }
+    }
+
+}
 
 int main(){
     std::string ficFilm="film.txt";
     std::string ficSelection="selection.txt";
-    Selection selection;
+    Selection selection1,selection2;
     EnsFilm ensFilm;
 
-    //selection.adrTabFilm = &(ensFilm.tabFilm);
+    //selection1.adrTabFilm = &(ensFilm.tabFilm);
 
     lectureFic(ensFilm,ficFilm);
     affEnsFilm(ensFilm);
 
-    selectionFilm(selection, ensFilm);
-    affSelection(selection);
+    selectionFilm(selection1, ensFilm);
+    affSelection(selection1);
 
-    enregistrerSelection(selection, ficSelection);
+    // enregistrerSelection(selection1, ficSelection);
 
+    selectionFilm(selection2, ensFilm);
+    affSelection(selection2);
+
+    std::cout<<"Il y'a "<<nbFilmCommuns(selection1, selection2)<<" film(s) en commun(s) entre les deux selections"<<std::endl;
+
+    triSelection(selection1);
+    affSelection(selection1);
 
     return 0;
 }
