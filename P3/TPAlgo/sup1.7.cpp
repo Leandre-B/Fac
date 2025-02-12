0d7bae425d195842de18f0ed76e8b095cea674fb
Léandre <leandrebodo@gmail.com>
2025-02-12 18:26:52 +0100

    Update sup1.7.cpp

--- P3/TPAlgo/sup1.7.cpp
+++ P3/TPAlgo/sup1.7.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 std::string extrait (std::string e, int pos, int lg){
-    if(lg<0) return "";
+    if(lg<1) return "";
     else return(extrait(e,pos,lg-1)+e[pos+lg-1]);
 }
 
@@ -32,33 +32,33 @@ int parent_ouvertes (std::string e){
     if(e.length()<=0)
         return 0;
     else{
-        if(e[e.length()-1]=='(') return parent_ouvertes(extrait(e,1,e.length()-2)) +1;
-        if(e[e.length()-1]==')') return parent_ouvertes(extrait(e,1,e.length()-2)) -1;
-        else return parent_ouvertes(extrait(e,1,e.length()-2));
+        if(e[e.length()-1]=='(') return parent_ouvertes(extrait(e,0,e.length()-1)) +1;
+        if(e[e.length()-1]==')') return parent_ouvertes(extrait(e,0,e.length()-1)) -1;
+        else return parent_ouvertes(extrait(e,0,e.length()-1));
     }
 
 }
 
 int cherchesigneMult(std::string e, unsigned int pos){
 
-    if(pos>=e.length())
+    if(pos>e.length())
         return -1;
     else{
-        if(parent_ouvertes (extrait(e,1,pos))==0)
+        if(parent_ouvertes (extrait(e,0,pos+1))==0)
             if(e[pos]=='/' or e[pos]=='*' or e [pos]=='%')
-                return pos+1;
+                return pos;
         return cherchesigneMult(e,pos+1);
     }
 }
 
 int cherchesigne(std::string e, unsigned int pos){
 
-    if(pos>=e.length())
+    if(pos>e.length())
         return cherchesigneMult(e,0);
     else{
-        if(parent_ouvertes (extrait(e,1,pos))==0)
+        if(parent_ouvertes (extrait(e,0,pos+1))==0)
             if(e[pos]=='-' or e[pos]=='+')
-                return pos+1;
+                return pos;
         return cherchesigne(e,pos+1);
     }
 }
@@ -74,19 +74,52 @@ int puiss(int nb,int p){
 }
 
 
-int convertit (std::string n, int ind){
-    if(ind==1)
-        return num(n[ind-1])*puiss(10,n.length());
-    else return (convertit(n,ind-1)+ num(n[ind-1])*puiss(10,n.length()-ind-1));
+int convertit (std::string n, int nb){
+    if(nb==1){
+        return num(n[nb-1])*puiss(10,n.length()-nb);
+    }
+    else return (convertit(n,nb-1)+ num(n[nb-1])*puiss(10,n.length()-nb));
+}
+
+
+int calcule (int g, char signe, int d){
+        switch (signe){
+            case '+' : return g + d;
+            case '-' : return g - d;
+            case '*' : return g * d;
+            case '/' : return g / d;
+            case '%' : return g % d;
+            default : return 0;
+        }
+}
+
+int evalue (std::string e){
+    if(e[0]=='(' and e[e.length()-1]==')')
+        return evalue(extrait(e,1,e.length()-2));
+    else{
+        int posSigne = cherchesigne(e,0);
+        if(posSigne!=-1){
+            std::string g=extrait(e,0,posSigne);
+            std::string d=extrait(e,posSigne+1,e.length()-posSigne-1);
+            std::cout<<"signe : "<<e[posSigne]<<" | g="<<g<<" | d="<<d<<std::endl;
+            return calcule(evalue(g),e[posSigne],evalue(d));
+        }
+        else{
+            int e_conv = convertit(e,e.length());
+            // std::cout<<"e.length : "<<e.length()<<" | str : \""<<e<<"\" = int :"<<e_conv<<std::endl;
+            // std::cout<<"Dernier char de la chaine \""<<e<<"\" : "<<e[e.length()-1]<<std::endl;
+            return e_conv;
+        }
+    }
 }
 
 int main(){
 
-    std::string e = "45/(5+4)*23";
-    // std::cout<<extrait(e,0,4)<<std::endl; //-1 sur lg car e[pos] compris
+    std::string e = "5+(1+4)+1+(3-4)";
+    // std::cout<<extrait(e,0,3)<<std::endl;
     //std::cout<<est_signe('5')<<est_signe('/')<<est_chiffre('5')<<est_chiffre('t');
-    // std::cout<<"Signe add prio : "<<cherchesigne(e,0)<<std::endl;
-    std::cout<<convertit("123",3)<<std::endl;
+    // std::cout<<"Signe prio : "<<cherchesigne(e,0)<<std::endl;
+    std::cout<<e<<" = "<<std::endl<<evalue(e)<<std::endl;
 
 
 
