9e0819569136c6fc9652fe7d0d58b7a7213e01d3
Léandre <leandrebodo@gmail.com>
2025-03-13 09:36:54 +0100

    Update cavalier.py

--- P4/Python/TP5/cavalier.py
+++ P4/Python/TP5/cavalier.py
@@ -3,6 +3,14 @@ deplacement_y=[-2, -2, 1, -1, 2,  2, -1, 1]
 dim_echequier = 5
 echequier = [[0]*dim_echequier for _ in range(dim_echequier)]
 nb_sol=0
+trouve = False
+
+def aff(echequier) :
+    for case in echequier :
+        print (f'{case[0]:>2} | {case[1]:>2} | {case[2]:>2} | {case[3]:>2} | {case[4]:>2}')
+        print('----------------------')
+
+
 
 def position_valide(x,y) -> bool :
     if 0<=x<dim_echequier and 0<=y<dim_echequier: 
@@ -26,22 +34,28 @@ def trouve_sol(x,y,etape) :
 
 
 def trouve_uniq_sol(x,y,etape) :
+    global trouve
+    global nb_sol
     global echequier
-    if(etape <= dim_echequier*dim_echequier+1):
-        for i in range(len(deplacement_x)) :
-            next_x=x+deplacement_x[i]
-            next_y=y+deplacement_y[i]
-            if(position_valide(next_x,next_y)) : 
-                if(echequier[next_x][next_y]==0) :
-                    echequier[next_x][next_y] = etape
-                    trouve_sol(next_x,next_y,etape+1)
-                    echequier[next_x][next_y]=0
-
+    
+    if(etape == dim_echequier*dim_echequier+1):
+        aff(echequier)
+        trouve = True
+        nb_sol+=1
+    elif not trouve :
+            for i in range(len(deplacement_x)) :
+                next_x=x+deplacement_x[i]
+                next_y=y+deplacement_y[i]
+                if(position_valide(next_x,next_y)) : 
+                    if(echequier[next_x][next_y]==0) :
+                        echequier[next_x][next_y] = etape
+                        trouve_uniq_sol(next_x,next_y,etape+1)
+                        echequier[next_x][next_y]=0
+    
 def main() : 
     echequier[0][0]=1
     trouve_uniq_sol(0,0,2)
-    print (f'{echequier[0]}\n{echequier[1]}\n{echequier[2]}\n{echequier[3]}\n{echequier[4]}\n')
-    #print(f'Nombre de sol : {nb_sol}')
+    print(nb_sol)
 
 
 
