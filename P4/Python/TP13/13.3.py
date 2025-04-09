e34c56fbce983d9cf2dc140c01480504ebe2a257
Leandre-B <leandrebodo@gmail.com>
2025-04-09 18:03:22 +0200

    updata TP13 Python P4

--- P4/Python/TP13/13.3.py
+++ P4/Python/TP13/13.3.py
@@ -5,15 +5,13 @@ def permute(s:str) -> list :
 		return list(s)
 	else:
 		result = []
-		for c in s:
-			s_wc = s.replace(c,'')
-			print(f'{c} + permu {s_wc}')
-			result=result+(permute(s_wc))
-			#result+=(permute(s)).remove(c)
-			print(f'{result}')
-			for ch in result :
-				result[i]=c+result[i]
-		return result
+		for i in range(len(s)):
+			c = s[i]
+			reste = s[:i] + s[i+1:]
+			sous_permu = permute(reste)
+			for ch in sous_permu:
+					result.append(c + ch)
+	return result
 
 def main() : 
 	print(permute('abc'))
