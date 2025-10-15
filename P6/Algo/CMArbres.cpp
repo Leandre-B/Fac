8e58d3b63c0c79e0049fbd70462ab9a9547d459b
Leandre-B <leandrebodo@gmail.com[B>
2025-10-15 10:04:17 +0200

    P6 Algo - ajout fonction arbre binaire (CM)

--- P6/Algo/CMArbres.cpp
+++ P6/Algo/CMArbres.cpp
@@ -14,14 +14,24 @@ struct Noeud{
 using Arbre =  Noeud*;
 
 
+void affPile(std::stack<Arbre> pile){
+    std::cout<<"Pile : \n";
+    while(!pile.empty()){
+        std::cout<<pile.top()->e;
+        pile.pop();
+    }
+    std::cout<<"\n\n";
+}
+
+
 Arbre creeArbre (const Element & e, const Arbre & gauche, const Arbre & droit){
     Arbre A = new Noeud;
     A->e = e;
     A->sag = gauche;
     A->sad = droit;
-
+    
     return A;
-}
+}    
 
 
 //====CREATION ARBRES====
@@ -62,14 +72,10 @@ Arbre construireArbre1(){
 
 //===Recursifs===
 void prefixeRecur(Arbre arb){
-    if(arb==nullptr)
-        std::cout<<'v';
-    else{
-        std::cout<<'('<<arb->e<<",";
+    if(arb!=nullptr){
+        std::cout<<arb->e<<" ";
         prefixeRecur(arb->sag);
-        std::cout<<',';
         prefixeRecur(arb->sad);
-        std::cout<<')';
     }
 }
 
@@ -84,10 +90,17 @@ void infixeRecur(Arbre arb){
 void postfixeRecur(Arbre arb){
     if(arb!=nullptr){
         postfixeRecur(arb->sag);
-        std::cout<<arb->e<<" ";
         postfixeRecur(arb->sad);
+        std::cout<<arb->e<<" ";
     }
 }
+//jsp mdr
+//
+//void lageurRecur(Arbre arb){
+//    if(arb!=nullptr){
+//
+//    }
+//}
 //===Fin Recursifs===
 
 
@@ -107,22 +120,70 @@ void prefixeIte(Arbre arb){
     }
 }
 
-void infixeIte(Arbre arb){
+void infixeIte(Arbre arb){ //marche pas :(
     std::stack<Arbre> pile;
     pile.push(arb);
-    do{
-
-        if(arb->sag!=nullptr)
-            pile.push(arb->sag);
-
-        std::cout<<pile.top()->e;
-        pile.pop();
-
-        if(arb->sad!=nullptr)
-            pile.push(arb->sad);
-    }while(!pile.empty());
+    while(!pile.empty()){
+        if(pile.top()->sag!=nullptr)
+        {
+            pile.push(pile.top()->sag);
+            //affPile(pile);
+        }
+        else
+        {
+            Arbre cur = pile.top();
+            pile.pop();
+            std::cout<<cur->e<<" ";
+            if(cur->sad!=nullptr)
+                pile.push(cur->sad);
+        }
+    }
 }
 
+void lageurIte(Arbre arb){
+    //AVEC **QUEUE**
+    std::queue<Arbre> file;
+    file.push(arb);
+    while(!file.empty()){
+        if(file.front()->sag!=nullptr)
+            file.push(file.front()->sag);
+        if(file.front()->sad!=nullptr)
+            file.push(file.front()->sad);
+        std::cout<<file.front()->e<<" ";
+        file.pop();
+    }
+}
+
+//jsp test bête
+
+//void prefixeRecur(Arbre arb){    
+//    if(arb!=nullptr){             Ligne 1
+//        std::cout<<arb->e<<" ";   Ligne 2
+//        prefixeRecur(arb->sag);   Ligne 3 on push puis reviens ligne 1
+//        prefixeRecur(arb->sad);   Ligne 4 on push puis reviens ligne 2
+//    }                             Ligne 5 on pop, si l3->l2, si l4->l1  
+//}
+void ite(Arbre arb){ //marche pas :(
+    int ligne=3;
+    std::stack<Arbre> pile;
+    pile.push(arb);
+
+    while(!pile.empty()){
+        if(pile.top()==nullptr){
+            pile.pop();
+            if(ligne==3) ++ligne;
+            else if (ligne==4) ligne = 3;
+        }
+        else{
+            std::cout<<pile.top()->e;
+            if(ligne==3)
+                pile.push(pile.top()->sag);
+            else if(ligne==4)
+                pile.push(pile.top()->sad);
+            pile.pop();
+        }
+    }
+}
 //===Fin Itératif===
 
 //====Fin Affichage====
@@ -143,6 +204,8 @@ int main(){
     //affichage(postfixeRecur,arb, "Postfixe");
     //affichage(prefixeIte,arb,"Prefixe Ite");
     affichage(infixeIte,arb,"Infixe Ite");
+    //affichage(lageurIte,arb, "Largeur Ite");
+    //affichage(ite,arb,"Test ite");
 
     return  0;
 }
