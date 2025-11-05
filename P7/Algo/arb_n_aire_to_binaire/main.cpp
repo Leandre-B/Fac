62855ebde52644f7c7c9d78af9129782765da9ad
Leandre-B <contact@leandre-b.com>
2025-11-05 15:34:06 +0100

    debut arb na aire en binaire

--- P7/Algo/arb_n_aire_to_binaire/main.cpp
+++ P7/Algo/arb_n_aire_to_binaire/main.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
 
+
+struct Noeud{
+    int elem;
+    Noeud* fils_aine;
+    Noeud* frere_droit;
+};
+
+using Arbre = Noeud*;
+
+Noeud* construireNoeud(int elem, Noeud* fils_aine, Noeud* frere_droit){
+    Noeud* noeud = new Noeud;
+    noeud->elem = elem;
+    noeud->fils_aine = fils_aine;
+    noeud->frere_droit = frere_droit;
+
+    return noeud;
+}
+
+Arbre construireArbre(){
+    Noeud* v = nullptr;
+    Arbre L = construireNoeud(12,v,v);
+    Arbre K = construireNoeud(11,v,L);
+    Arbre J = construireNoeud(10,v,K);
+    Arbre I = construireNoeud(9,v,J);
+    Arbre G = construireNoeud(7,I,v);
+    Arbre F = construireNoeud(6,v,v);
+    Arbre H = construireNoeud(8,v,v);
+    Arbre E = construireNoeud(5,H,F);
+    Arbre D = construireNoeud(4,v,v);
+    Arbre C = construireNoeud(3,G,D);
+    Arbre B = construireNoeud(2,E,C);
+    Arbre A = construireNoeud(1,B,v);
+
+    return A;
+}
+
+//C'est le meme parcours prefixe OMG
+void affPrefix(Arbre arb){
+    if(arb!=nullptr){
+        std::cout<<arb->elem<<" ";
+        affPrefix(arb->fils_aine);
+        affPrefix(arb->frere_droit);
+    }
+}
+
 int main(){
-        return 0;
+    Arbre arb = construireArbre();
+    affPrefix(arb);te
+
+    return 0;
 }
