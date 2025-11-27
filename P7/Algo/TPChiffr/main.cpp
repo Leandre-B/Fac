25bdff52d9b0eb614a2b085813dc6afa77247499
Bodo Leandre <leand@fixe-g103-02>
2025-11-27 10:49:33 +0100

    suite tp chiffr Algo P7

--- P7/Algo/TPChiffr/main.cpp
+++ P7/Algo/TPChiffr/main.cpp
@@ -83,7 +83,107 @@ void afficheCle(const Cle & cle){
 
 //=============
 
+//======= Chemin =========
+struct MaillonBool{
+	bool val;
+	MaillonBool* suiv;
+};
 
+using Chemin = MaillonBool*;
+
+void construireBis(Chemin & chemin, unsigned int taille){
+	if(taille==0 or taille==1){
+		chemin = new MaillonBool;
+		chemin->val = taille!=0;
+		chemin->suiv=nullptr;
+	}else{
+		chemin = new MaillonBool;
+		chemin->val = taille%2!=0;
+		construireBis(chemin->suiv, taille/2);
+	}
+}
+
+Chemin construireChemin(unsigned int taille){
+	Chemin chemin = nullptr;
+	construireBis(chemin, taille);
+	return chemin;
+}
+
+void afficheCheminBis(Chemin chemin){
+	if(chemin!=nullptr){
+		if(chemin->val)
+			std::cout<<'F';
+		else
+			std::cout<<'T';
+		afficheCheminBis(chemin->suiv);
+	}
+}
+
+void afficheChemin(Chemin chemin){
+	afficheCheminBis(chemin);
+	std::cout<<std::endl;
+}
+void supprimeChemin(Chemin & chemin){
+	if(chemin!=nullptr){
+		Chemin bis = chemin;
+		chemin = chemin->suiv;
+		delete bis;
+		supprimeChemin(chemin);
+	}
+}
+//===============
+
+//===== Message Chiffre =======
+struct NoeudInt{
+	unsigned int val;
+	NoeudInt* sag;
+	NoeudInt* sad;
+};
+
+using MessageChiff = NoeudInt*;
+
+void ajoutNombre(MessageChiff & m, Chemin chemin, const unsigned int val){
+	if(m==nullptr){
+		m= new NoeudInt;
+		m->val = val;
+		m->sad = nullptr;
+		m->sag = nullptr;
+	}else if(chemin==nullptr)
+		ajoutNombre(m->sag, chemin, val);
+	else{
+		if(chemin->val)
+			ajoutNombre(m->sad, chemin->suiv, val);
+		else
+			ajoutNombre(m->sag, chemin->suiv, val);
+	}
+}
+
+
+
+void afficheMessageChiff(MessageChiff m){
+	if(m!=nullptr){
+		afficheMessageChiff(m->sag);
+		std::cout<<(m->val)<<" ";
+		afficheMessageChiff(m->sad);
+	}
+}
+
+void supprimeFeuille(MessageChiff & m, Chemin chemin){
+	//si c'est un feuille
+	if(m->sag == m->sad){
+		delete m;
+		m=nullptr;
+	}else if(chemin==nullptr)
+		supprimeFeuille(m->sag, chemin);
+	else{
+		if(chemin->val)
+			supprimeFeuille(m->sad, chemin->suiv);
+		else
+			supprimeFeuille(m->sag, chemin->suiv);
+	}
+}
+
+//=====================
 int main(){
 	srand(time(nullptr));
 
@@ -92,6 +192,18 @@ int main(){
 
 	Cle cle = genCleAlea(10);
 	afficheCle(cle);
+	
+	Chemin chemin = construireChemin(8);
+	afficheChemin(chemin);
+
+	MessageChiff messageChiff = nullptr;
+	ajoutNombre(messageChiff, chemin, 10);
+	ajoutNombre(messageChiff, chemin, 2);
+	ajoutNombre(messageChiff, chemin, 1);
+	ajoutNombre(messageChiff, chemin, 1203);
+	afficheMessageChiff(messageChiff);
+
+	supprimeChemin(chemin);
 
 	return 0;
 }
