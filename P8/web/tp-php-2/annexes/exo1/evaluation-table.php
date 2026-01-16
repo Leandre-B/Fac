c6bcbca2e7b575ed9daee2d0bc1dd738fecaff9c
Bodo Leandre <leand@fixe-g104-03>
2026-01-16 16:48:21 +0100

    ex1 TP2 web + debut ex2

--- P8/web/tp-php-2/annexes/exo1/evaluation-table.php
+++ P8/web/tp-php-2/annexes/exo1/evaluation-table.php
@@ -22,7 +22,43 @@
 </head>
 <body>
 <?php
-// A COMPLETER
+    $val = [
+        "TRUE" => TRUE,
+        "FALSE" => FALSE,
+        "1" =>1,
+        "0" =>0,
+        "-1" =>-1,
+        "\"1\"" => "1",
+        "\"0\"" => "0",
+        "\"-1\"" => "-1",
+        "NULL" => NULL,
+        "[]" => [],
+        "\"\"" => ""
+    ];
+
+    $tabComparaison = [];
+    foreach($val as $k1=>$v1){
+        $ligne = [];
+        foreach($val as $k2=>$v2){
+            $ligne[$k2] = ($v1 == $v2);
+        }
+        $tabComparaison[$k1] = $ligne;
+    }
+
+    echo "<table>";
+    echo "<th>==";
+    foreach($tabComparaison as $k1=>$l){
+        echo "<th>$k1";
+    }
+    foreach($tabComparaison as $k1=>$l){
+        echo "<tr>";
+        echo "<th>$k1";
+        foreach($l as $k2=>$v){
+            echo "<td>";
+            if($v)
+                echo "TRUE";
+        }
+    }
 ?>
 </body>
 </html>
