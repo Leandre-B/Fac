b1ab39495b3eb4b446d6b5d4537c736f3bd65345
Bodo Leandre <leand@fixe-g103-02>
2026-01-27 15:25:28 +0100

    suite tp4 web

--- P8/web/tp-php-4/annexes/exo1/calculatrice.php
+++ P8/web/tp-php-4/annexes/exo1/calculatrice.php
@@ -8,7 +8,24 @@
 
 <body>
     <!-- A INSERER : CODE PHP POUR TRAITER LE FORMULAIRE -->
+    <?php
+        $nb1=htmlspecialchars($_POST["nb1"] ?? 0);
+        $nb2=htmlspecialchars($_POST["nb2"] ?? 0);
+        $result=htmlspecialchars($_POST["result"] ?? 0);
+        $calcul=htmlspecialchars($_POST["calcul"] ?? "");
 
+        if($calcul==="Addition x+y")
+            $result = intval($nb1) + intval($nb2);
+        else if($calcul==="Soustraction x-y")
+            $result = intval($nb1) - intval($nb2);
+        else if($calcul==="Division x/y")
+            if(intval($nb2)!=0)
+                $result = intval($nb1) / intval($nb2);
+            else
+                $result = 0;
+        else if($calcul==="Puissance x^y")
+            $result = intval($nb1) ** intval($nb2);
+    ?>
     <h3>Entrez deux nombres : </h3>
     <!-- A MODIFIER : CODE PHP POUR AFFICHER ARGUMENTS ET RESULTAT EVENTUELS  -->
     <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
@@ -18,19 +35,19 @@
                 <tbody>
                     <tr>
                         <td><label><b> Nombre 1 </b></label> <input type="number" step="1" name="nb1" size="30"
-                                value="" />
+                                value="<?= $nb1 ?>" />
                         </td>
                     </tr>
                     <tr>
                         <td><label><b> Nombre 2 </b></label> <input type="number" step="1" name="nb2" size="30"
-                                value="" />
+                                value="<?= $nb2 ?>" />
                         </td>
                     </tr>
                     <tr>
                         <td><label><b> Résultat </b></label> <input type="number" step="1" name="result" size="30"
-                                value="" disabled />
+                                value="<?= $result ?>" disabled />
                         </td>
-                    </tr>
+                    </tr>   
                     <tr>
                         <td><label><b> Choisissez ! </b></label> <input type="submit" name="calcul"
                                 value="Addition x+y" />&nbsp;&nbsp;&nbsp;
@@ -43,5 +60,4 @@
             </table>
         </fieldset>
     </form>
-
 </html>
--- P8/web/tp-php-4/annexes/exo2/articles.php
+++ P8/web/tp-php-4/annexes/exo2/articles.php
@@ -22,7 +22,7 @@
 		<!-- A MODIFIER POUR, LE CAS ECHEANT, RE-AFFICHER LE MOTIF ET L'OPTION CHOISIE -->
 		<input type="text" name="motif"/>
 		<label for="auteur">Par auteur/année</label>
-		<input type="radio" name="cible" id="auteur" value="auteur"/>
+		<input type="radio" name="cible" id="auteur" value="auteur" default/>
 		<label for="titre">Par titre</label>
 		<input type="radio" name="cible" id="titre" value="titre"/>
 		<input type="submit" name="submit" value="Rechercher">
@@ -31,7 +31,33 @@
 	<fieldset>
 	<legend><strong>Résultat</strong></legend>
     <?php
-	// A COMPLETER
+
+		$motif = "/".htmlspecialchars($_POST["motif"] ?? "*")."/";
+		echo $motif;
+		echo "<br><br><br><br>";
+
+		//preg_match($motif,$ch1,$result);
+		$choix = htmlspecialchars($_POST["cible"] ?? "");
+		echo $choix;
+
+		$fic = fopen("articles.txt", "r");
+		while ( ($ligne = fgets($fic)) !== FALSE )
+			$txt[]= $ligne;
+		//print_r($txt);
+		foreach ($txt as $v){
+			if($choix==="titre"){
+				preg_match("/\\[.*\\]/",$v,$a_rechercher_v);
+				$a_rechercher_v = $a_rechercher_v[0];
+				echo $a_rechercher_v;
+			}
+
+			if (preg_match($motif,$a_rechercher_v)==1){
+				echo $v;
+				echo "<br>";
+			}
+
+		}
+
     ?>
 	</fieldset>
 </body>
