b4c39d24e151da2631e6a5836edcc3fbd3d86f0a
Bodo Leandre <leand@fixe-g105-03>
2026-02-11 14:17:36 +0100

    fin tp7

--- P8/web/tp-php-7/annexes/exo1/api-wikipedia.php
+++ P8/web/tp-php-7/annexes/exo1/api-wikipedia.php
@@ -1,4 +1,31 @@
 <?php
-$apiUrl = 'https://fr.wikipedia.org/w/api.php';
-// A COMPLETER
-?>
+$apiUrl = 'https://fr.wikipedia.org/w/api.php?';
+$parameters = [
+    "action" => "query",
+    "format" => "json",
+    "titles" => "Angers",
+    "prop" => "extracts",
+    "exintro" => "true" ,
+    "explaintext" => "true",
+    "exsentences" => "5",
+];
+// Encodage des paramètres au format query-string
+$apiUrl .= http_build_query($parameters);
+
+// Envoi de la requête en HTTP GET par défaut
+$response = file_get_contents($apiUrl);
+
+if ($response === false)
+    echo "Erreur lors de l’envoi de la requête !";
+else {
+    echo $response;
+    // Conversion de la réponse au format JSON en tableau associatif
+    $tab = json_decode($response, TRUE);
+    print_r($tab);
+}
+/*
+https://fr.wikipedia.org/w/api.php?action=query&format=json&titles=Paris&prop=extracts&exintro=true&explaintext=true&exsentences=5
+
+target : 
+https://fr.wikipedia.org/w/api.php?action=query&format=json&titles=Paris&prop=extracts&exintro=true&explaintext=true&exsentences=5
+*/
--- P8/web/tp-php-7/annexes/exo2/api-adresses.php
+++ P8/web/tp-php-7/annexes/exo2/api-adresses.php
@@ -1,41 +1,57 @@
 <?php
 // en-tête de la réponse HTTP signifiant que ce fichier renvoie du JSON
+
 header( 'Content-Type: application/json' );
 // URL de l'API
 $apiUrl = 'https://api-adresse.data.gouv.fr/search/';
 
-////////////////////////
-// A COMPLETER PAS A PAS
-////////////////////////
-
 // Récupération du champ contenant les mots-clés relatifs aux adresses à rechercher
 
+if ( ! $rue = ( $_POST[ "rue" ] ?? "" ) ) {
+    die( "Aucun mot-clé fourni !" );
+}
+
+
 // Chemin vers le fichier INI contenant les 2 autres paramètres de la requête HTTP
+$ini_file="api-adresses.ini";
 
 // Lecture du fichier INI
+$ini_array = parse_ini_file($ini_file);
+//print_r($ini_array);
 
 // Tableau des paramètres de la chaîne de requête HTTP
+$url="https://api-adresse.data.gouv.fr/search/?";
+$parameters = [
+    "q"     => $rue,
+    "type"  => $ini_array["type"],
+    "limit" => $ini_array["limit"]
+];
 
 // Construction de la chaîne de requête
+$apiUrl .= "?".http_build_query($parameters);
+//echo $apiUrl;
 
 // Construction de l'URL complète
+$request=$apiUrl;
 
 // Envoi de la requête
+$response = file_get_contents($request);
 
 // Décodage de la réponse JSON en tableau associatif
+if ($response === false)
+    echo "Erreur lors de l’envoi de la requête !";
+else {
+    // Conversion de la réponse au format JSON en tableau associatif
+    $tab = json_decode($response, TRUE);
+    //print_r($tab["features"]);
+}
 
-// Filtrage du tableau et renvoi au format JSON du type :
-// [
-//     {
-//         "address": "rue Evariste Galois 29200 Brest",
-//         "lng": -4.481212,
-//         "lat": 48.413073
-//     },
-//     {
-//         "address": "Allée Evariste Galois 18000 Bourges",
-//         "lng": 2.357755,
-//         "lat": 47.077523
-//     },
-// ....
-/// ]
+foreach ($tab["features"] as $k=>$v){
+    $json[] = ["address" => $v["properties"]["label"], 
+               "lng" => $v["geometry"]["coordinates"][0],
+               "lat" => $v["geometry"]["coordinates"][1]];
+}
+
+print_r(json_encode($json));
+
 ?>
