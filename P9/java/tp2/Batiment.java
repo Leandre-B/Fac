579846ac8748badb35816ee83f934e8ffa1c908e
Bodo Leandre <leand@fixe-h001-02>
2026-03-31 16:46:03 +0200

    debut tp2

---
+++ P9/java/tp2/Batiment.java
@@ -0,0 +1,8 @@
+public class Batiment {
+    private String adresse;
+    private int surfaceHabitable;
+
+    public String toString(){
+        return "Classe : Batiment\n"+"Adresse : "+adresse+"\n+";
+    }
+}
