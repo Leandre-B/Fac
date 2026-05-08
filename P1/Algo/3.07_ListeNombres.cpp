822cb42a8d24395b582322ee32b5d8584f7612a2
Leandre-B <contact@leandre-b.com>
2026-05-08 17:09:30 +0200

    fix : non respect consigne

--- P1/Algo/3.07_ListeNombres.cpp
+++ P1/Algo/3.07_ListeNombres.cpp
@@ -5,8 +5,10 @@ int main(){
     for(int i=0;i<=9;++i){
         for(int j=0;j<=9;++j){
             for(int k=0;k<=9;++k){
-                if(i+j+k==9)
-                    std::cout<<i<<j<<k<<std::endl;
+                if(i+j+k==9 and i!=j and j!=k and k!=i){
+                    //std::cout<<i<<j<<k<<std::endl;
+                    std::cout<<i*100+j*10+k<<std::endl;
+                }
             }
         }
     }
