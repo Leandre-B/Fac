3b46e4cbc6a1ae68a25a34236fcc8c6fb6c98053
Leandre-B <contact@leandre-b.com>
2026-05-08 16:20:16 +0200

    fix : divsion entiere au lieu de réelle

--- P1/Algo/2.3_FonctionMath.cpp
+++ P1/Algo/2.3_FonctionMath.cpp
@@ -6,13 +6,13 @@ int main()
     std::cout << "Programme pour calculer f(x) en fontion de x";
     std::cout << "Saissiez x : ";
     std::cin >> x;
-    if((((2/3)*x*x)-6)==0.0)
+    if((((2.0/3)*x*x)-6)==0.0)
         std::cout << "ERREUR :  f(x) n'est pas definie pour x= " << x;
     else{
         if(x<0){
-            std::cout << "f(x)= " << (-x + 3 *x*x)/((2/3)*x*x-6) << std::endl;
+            std::cout << "f(x)= " << (-x + 3 *x*x)/((2.0/3)*x*x-6) << std::endl;
         }else{
-            std::cout << "f(x)= " << (x + 3 *x*x)/((2/3)*x*x-6) << std::endl;
+            std::cout << "f(x)= " << (x + 3 *x*x)/((2.0/3)*x*x-6) << std::endl;
         }
     }
 
