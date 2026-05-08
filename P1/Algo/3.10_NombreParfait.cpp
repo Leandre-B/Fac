0298097b628f43bcf489b750bd2cfe5bc1800242
Leandre-B <contact@leandre-b.com>
2026-05-08 17:14:21 +0200

    refractor : explicite cout

--- P1/Algo/3.10_NombreParfait.cpp
+++ P1/Algo/3.10_NombreParfait.cpp
@@ -4,9 +4,9 @@ int main(){
 
     int min,max,somme;
 
-    std::cout<<"Entrez la borne inférieure : ";
+    std::cout<<"Entrez la borne inférieure des nombres à vérifier : ";
     std::cin>>min;
-    std::cout<<"Entrez la borne inférieure : ";
+    std::cout<<"Entrez la borne supérieur des nombres à vérifier : ";
     std::cin>>max;
 
     for(int j=min;j<=max;++j){
