2ec6aaa44bb470293b59872fcfb5bce5e21b8feb
Leandre <176747677+Leandre-B@users.noreply.github.com>
2026-07-16 18:24:36 +0200

    remove sokoban file to link to the good repo

--- P5/Sokoban/src/graphique/menu.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-#include "selection_sokoban.h"
-
-extern const unsigned int LARGEUR_FENETRE;
-extern const unsigned HAUTEUR_FENETRE;
-
-bool ouvre_menu();
--- P5/Sokoban/src/graphique/selection_sokoban.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "jeu.h"
-#include "../plateau/get_plateau_xsb.h"
-#include "../plateau/get_plateau_json.h"
-
-#include <filesystem>
-
-bool ouvre_selection(sf::RenderWindow & fenetre,sf::Event & event, sf::View & view);
